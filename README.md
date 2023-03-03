8d9b6f2bb4a842063a2f22bd26dd38e199c45e45
Ronuel Joshua B. Reyes <ronuelreyes@gmail.com>
2023-03-03 13:36:42 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Digital_Dexterity_Backend
---
+++ digital_dex_admin_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DigitalDexAdminWebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'digital_dex_admin_web'
