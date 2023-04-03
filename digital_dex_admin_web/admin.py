b865c82080aed277d27243e36eb533dc426034c6
Ronuel Joshua B. Reyes <ronuelreyes@gmail.com>
2023-04-03 18:18:26 +0800

    03/04/2023 Added: 1. Tax Map Control Model Modified: 1. Admin

--- digital_dex_admin_web/admin.py
+++ digital_dex_admin_web/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models.admin_model import Admin
+from .models.tax_map_control_model import TaxMapControl
 
 admin.site.register(Admin)
+admin.site.register(TaxMapControl)
