9d4a5fa5f1384ba787962a32b81fdcebd04b03e1
markynuts <mark200154@gmail.com>
2023-05-03 21:17:15 +0800

    05/03/2023 Modified: 1. Serializers 2. Settings 3. Wsgi 4. Build Files 5. Vercel JSON

--- digital_dex_admin_web/versions/v1p0/features/exempt_assessment_roll/display_exempt_assessment/serializers/display_assessment_serializer.py
+++ digital_dex_admin_web/versions/v1p0/features/exempt_assessment_roll/display_exempt_assessment/serializers/display_assessment_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .......models.exempt_assessment_model import ExemptAssessment
 
 class DisplayAssessmentSerializer(serializers.ModelSerializer):
+    classification = serializers.CharField(source='get_classification_display')
     class Meta:
         model = ExemptAssessment
         fields = ['id','exempt_assessment_roll','arpn','td_no','pin','lot_block_no','property_owner',
--- digital_dex_admin_web/versions/v1p0/features/tax_declaration_form/create_tax_form/serializers/initial_assessment_serializer.py
+++ digital_dex_admin_web/versions/v1p0/features/tax_declaration_form/create_tax_form/serializers/initial_assessment_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .......models.tax_initial_assessment_model import InitialAssessment
 
 class InitialAssessmentSerializer(serializers.ModelSerializer):
+    classification = serializers.CharField(source='get_classification_display')
     class Meta:
         model = InitialAssessment
         fields = [
